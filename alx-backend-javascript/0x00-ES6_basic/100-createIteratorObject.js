5d1431a177301ce45acc55bea22395965a46c0c2
Ifuhad622 <ifuhad007@gmail>
2024-07-02 18:51:09 +0000

    0x03-ES6_data_manipulation

--- alx-backend-javascript/0x00-ES6_basic/100-createIteratorObject.js
+++
@@ -1,8 +0,0 @@
-export default function createIteratorObject(report) {
-  const myArray = [];
-  for (const item of Object.values(report.allEmployees)) {
-    myArray.push(...item);
-  }
-
-  return myArray;
-}
